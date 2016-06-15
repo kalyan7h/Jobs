50c500db9dcb4284c51e26139bf7e6876fc70916
unknown <kalyan7h@gmail.com>
2016-06-15 10:06:42 +0530

    updated to smoke suite

--- cmd.bat
+++ cmd.bat
@@ -1,1 +1,1 @@
-"%SEGUE_HOME%\partner.exe" -opt "C:\automation\ApplicationSpecific\Includes\quickendesktopoptionset.opt" -r "C:\automation\ApplicationSpecific\Scripts\Regression\Loans\LoansPart1.t"
+"%SEGUE_HOME%\partner.exe" -opt "C:\automation\ApplicationSpecific\Includes\quickendesktopoptionset.opt" -r "C:\automation\ApplicationSpecific\Scripts\Global\SmokeQuicken.t"
